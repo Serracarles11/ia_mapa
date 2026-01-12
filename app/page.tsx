ae24e6685da47202c9cd93f2dec344aa185640aa
Serracarles11 <Carlesserra685@gmail.com>
2026-01-12 18:08:55 +0100

    tyfdsad

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,23 @@
-import { Suspense } from "react"
-import PageClient from "@/components/PageClient"
+import { AppSidebar } from "@/components/app-sidebar"
+import { ChartAreaInteractive } from "@/components/chart-area-interactive"
+import { DataTable } from "@/components/data-table"
+import { SectionCards } from "@/components/section-cards"
+import { SiteHeader } from "@/components/site-header"
+import {
+  SidebarInset,
+  SidebarProvider,
+} from "@/components/ui/sidebar"
+import Map from "@/components/Map"
+
+
+import data from "../app/dashboard/data.json"
 
 export default function Page() {
   return (
-    <Suspense
-      fallback={<div className="min-h-screen bg-[#f4f3ef]" aria-hidden />}
-    >
-      <PageClient />
-    </Suspense>
+    <main className="h-screen p-4">
+      <div className="h-full rounded-lg overflow-hidden border">
+        <Map />
+      </div>
+    </main>
   )
 }
